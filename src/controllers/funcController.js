1b4a3c71b795ad1bdf8877bacc9e0f7be7f0e2f3
Vitor Eto <vitoreto07@gmail.com>
2024-11-15 11:45:31 -0300

    concertar o editar undefined

--- src/controllers/funcController.js
+++ src/controllers/funcController.js
@@ -1,12 +1,18 @@
 const controller = {};
 
 controller.list = (req, res) => {
+    const funcionarioIndex = parseInt(req.query.funcionarioIndex) || 0;
     req.getConnection((err, conn) => {
         if(err) return res.status(500).json({error: "Erro ao conectar ao banco de dados"});
             conn.query('SELECT * FROM funcionarios', (err, funcionarios) => {
                 if (err) return res.status(500).json({error: "Erro ao encontrar funcionário"});
-                res.render('funcionarios', {
-                    data: funcionarios
+                const funcionario = funcionarios[funcionarioIndex];
+                res.render('funcBusca', {
+                    data: funcionarios,
+                    funcionarioAtual: funcionario,
+                    clienteIndex: funcionarioIndex,
+                    totalFuncionarios: funcionarios.length,
+                    editar: false
                 });
             });
     });
@@ -34,6 +40,90 @@ controller.save = (req, res) => {
     });
 };
 
+
+controller.next = (req, res) => {
+    const funcionarioIndex = parseInt(req.query.funcionarioIndex) + 1; // Incrementa o índice
+    req.getConnection((err, conn) => {
+        if (err) return res.status(500).json('Erro ao conectar ao banco de dados');
+
+        conn.query('SELECT * FROM funcionarios', (err, funcionarios) => {
+            if (err) {
+                return res.status(500).send('Erro ao consultar os funcionarios');
+            }
+
+            // Se o índice for maior que o número total de funcionarios, volta para o primeiro
+            if (funcionarioIndex >= funcionarios.length) {
+                return res.redirect(`/funcionarios?funcionarioIndex=0&totalFuncionarios=${funcionarios.length}`);
+            }
+
+            // Redireciona para o próximo funcionario
+            res.redirect(`/funcionarios?funcionarioIndex=${funcionarioIndex}&totalFuncionarios=${funcionarios.length}`);
+        });
+    });
+};
+
+controller.prev = (req, res) => {
+    const funcionarioIndex = parseInt(req.query.funcionarioIndex) - 1; // Decrementa o índice
+    req.getConnection((err, conn) => {
+        if (err) {
+            return res.status(500).send('Erro ao conectar ao banco de dados');
+        }
+
+        conn.query('SELECT * FROM funcionarios', (err, funcionarios) => {
+            if (err) return res.status(500).json('Erro ao consultar os funcionarios');
+            const totalFuncionarios = funcionarios.length;
+            // Se o índice for menor que 0, volta para o último funcionario
+            if (funcionarioIndex < 0) {
+                return res.redirect(`/funcionarios?funcionarioIndex=${funcionarios.length - 1}&totalFuncionarios=${totalFuncionarios}`);
+            }
+
+
+            // Redireciona para o funcionario anterior
+            res.redirect(`/funcionarios?funcionarioIndex=${funcionarioIndex}&totalFuncionarios=${totalFuncionarios}`);
+        });
+    });
+};
+
+controller.edit = (req, res) => {
+    const { funcMatricula } = req.params;
+    req.getConnection((err, conn) => {
+        if (err) return res.status(500).json({ error: "Erro ao conectar ao banco de dados" });
+        conn.query('SELECT * FROM funcionarios WHERE funcMatricula = ?', [funcMatricula], (err, funcionario) => {
+            if (err) return res.status(500).json({ error: "Erro ao encontrar ao consultar matrícula do funcionario" });
+            res.render('funcBusca', {
+                funcionarioAtual: cliente[0],
+                funcionarioIndex: req.query.funcionarioIndex || 0,
+                totalFuncionarios: 1,
+                editar: true
+            });
+        });
+    });
+};
+
+controller.delete = (req, res) => {
+    const { funcMatricula } = req.params;
+    req.getConnection((err, conn) => {
+        if (err) return res.status(500).json({ error: "Erro ao concetar ao banco de dados" });
+        conn.query('DELETE FROM funcionarios WHERE funcMatricula = ?', [funcMatricula], (err, rows) => {
+            if (err) return res.status(500).json({ error: "Erro ao remover funcionario" });
+            res.redirect('/funcBusca');
+        });
+    });
+};
+
+
+controller.update = (req, res) => {
+    const { funcMatricula } = req.params;
+    const novoFuncionario = req.body;
+    const funcionarioIndex = req.body.funcionarioIndex || 0; // pega o funcionarioIndex enviado pelo formulário
+    req.getConnection((err, conn) => {
+        if (err) return res.status(500).json({ error: "Erro ao concetar ao banco de dados" });
+        conn.query('UPDATE funcionarios set ? WHERE funcMatricula = ?', [novoFuncionario, funcMatricula], (err, funcionario) => {
+            if (err) return res.status(500).json({ error: "Erro ao atualizar funcionário" });
+            res.redirect(`/funcionarios?funcionarioIndex=${funcionarioIndex}`); // redirecione para o funcionario atualizado
+        });
+    });
+};
 
 
 
--- src/routers/funcRoutes.js
+++ src/routers/funcRoutes.js
@@ -5,8 +5,20 @@ const funcController = require('../controllers/funcController');
 
 router.get('/funcionarios', funcController.list);
 
+router.get('/funcBusca', (req, res) => {
+    res.render('funcBusca');
+});
 
 router.post('/funcionarios/save', funcController.save);
 
+router.get('/funcionarios/next', funcController.next);
+
+router.get('/funcionarios/prev', funcController.prev);
+
+router.get('/funcionarios/delete/:funcMatricula', funcController.delete);
+
+router.get('/funcionarios/update/:funcMatricula', funcController.edit);
+      
+router.post('/funcionarios/update/:funcMatricula', funcController.update);
 
 module.exports = router;
